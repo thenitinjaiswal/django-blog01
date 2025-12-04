ed61f2bdabc9174bc1b145d2746af5b279c76734
Nitin Jaiswal <nitinjaiswal242004@gmail.com>
2025-12-04 18:52:25 +0530

    Remove pycache from tracking

--- myblog/settings.py
+++ myblog/settings.py
@@ -11,18 +11,25 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import os  # ← ADD THIS LINE
 
-
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key-change-this')
+SECRET_KEY = "django-insecure-=t)0ah4%38#f=%brflj^o(yc2rkoqxu%j*c*r!rl_&d&ojlf+("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']  # Will restrict this per platform
+ALLOWED_HOSTS = []
+
+
+
+
 
 # Application definition
 
@@ -34,7 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'blog',
-    'taggit'
+
+    'taggit',
     
 ]
 
@@ -114,17 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
 
-# Directory where collectstatic will collect static files
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Additional directories to look for static files
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
-# Media files (user uploads)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# REQUIRED FOR collectstatic
+STATIC_ROOT = BASE_DIR / "staticfiles"
